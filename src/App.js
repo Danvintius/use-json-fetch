3e7d5fd5b5a20626891df551529a55d52db3e477
Danvintius <danila.94@mail.ru>
2024-02-01 20:11:05 +0300

    1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Fragment} from 'react';
+import useJsonFetch from '../hooks/useJsonFetch'
 
-function App() {
+
+function ScopeHook({url}) {
+  const [data, loading, error] = useJsonFetch(url)
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>{data && `data.status - ${data.status}`}</p>
+      <p>{loading && `Loading...`}</p>
+      <p>{`Error` && error}</p>
     </div>
-  );
+  )
 }
 
-export default App;
+export default function App() {
+
+  return (
+    <Fragment>
+      <ScopeHook url={`${process.env.REACT_APP_DATA_URL}/data`}/>
+      <ScopeHook url={`${process.env.REACT_APP_DATA_URL}/loading`} />
+      <ScopeHook url={`${process.env.REACT_APP_DATA_URL}/error`} />
+    </Fragment>
+  );
+}
